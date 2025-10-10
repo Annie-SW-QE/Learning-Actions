6b751f02649e055c9650c4892bdee5fc0920be2f
Annie-SW-QE <woodard.anna@outlook.com>
2025-10-10 19:20:49 -0400

    add file write

--- new_add.py
+++ new_add.py
@@ -1,1 +1,6 @@
-print("I'm a barbie girl, I hate this stupid wooooorrrrllld")
+phrase = "I'm a barbie girl, I hate this stupid wooooorrrrllld"
+print(phrase)
+
+with open("printed_text.txt","w") as f:
+    f.write(phrase)
+    f.close()
