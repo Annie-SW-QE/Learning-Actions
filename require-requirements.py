df4568c6793416b2990577d76dab3029a2c8c423
Annie-SW-QE <woodard.anna@outlook.com>
2025-10-10 22:26:12 -0400

    check workflow edit

--- require-requirements.py
+++ require-requirements.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import os
 numbers = []
 
 for i in range(0,20,1):
     numbers.append(i)
 
 numbers_df = pd.DataFrame(numbers)
+os.mkdir("artifacts")
 
+numbers_df.to_excel("artifacts/numarr.xlsx")
