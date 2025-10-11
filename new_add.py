bd2e32cfc37b8e180316378772db2617d0dfeff2
Annie-SW-QE <woodard.anna@outlook.com>
2025-10-10 20:48:34 -0400

    change slash to try again

--- new_add.py
+++ new_add.py
@@ -2,7 +2,7 @@ import os
 phrase = "I am a barbie girl. I hate this stupid wooooorrrrllld"
 print(phrase)
 os.makedirs("artifacts")
-with open("artifacts\printed_text.txt","w") as f:
+with open("artifacts/printed_text.txt","w") as f:
     f.write(phrase)
     f.close()
     
