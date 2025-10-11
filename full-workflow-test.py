c6646a50028744c519a738c60a8f1d45ed0960b0
Annie-SW-QE <woodard.anna@outlook.com>
2025-10-11 16:07:41 -0400

    full workflow trigger

---
+++ full-workflow-test.py
@@ -0,0 +1,11 @@
+## Test the full workflow, including using secrets and writing artifacts to a folder
+import os
+import pandas as pd
+where = os.environ.get('my_var')
+my_array = []
+for i in range(0,20,1):
+    my_array.append(where)
+
+my_df = pd.DataFrame(my_array)
+os.mkdir("artifacts")
+my_df.to_excel("artifacts/my_excel.xlsx")
