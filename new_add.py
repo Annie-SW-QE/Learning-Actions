ddd2b5581fd041ff9668baa068c4698383419455
Annie-SW-QE <woodard.anna@outlook.com>
2025-10-10 20:41:35 -0400

    folder not working yet

--- new_add.py
+++ new_add.py
@@ -1,5 +1,5 @@
 import os
-phrase = "I am a barbie girl I hate this stupid wooooorrrrllld"
+phrase = "I am a barbie girl. I hate this stupid wooooorrrrllld"
 print(phrase)
 os.makedirs("artifacts")
 with open("artifacts\printed_text.txt","w") as f:
