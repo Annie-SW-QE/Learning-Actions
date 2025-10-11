52b71678c9166b66ca00c6d18b3efea584c4aa4a
Annie-SW-QE <woodard.anna@outlook.com>
2025-10-11 17:00:32 -0400

    please work

--- full-workflow-test.py
+++ full-workflow-test.py
@@ -1,12 +1,14 @@
 ## Test the full workflow, including using secrets and writing artifacts to a folder
 import os
 import pandas as pd
-where = os.environ.get('my_var')
+# where = os.environ.get('my_var')
+where = "theres"
+
 my_array = []
 for i in range(0,20,1):
     my_array.append(where)
-
-my_df = pd.DataFrame(my_array)
 os.makedirs("artifacts")
+my_df = pd.DataFrame(my_array)
+
 my_df.to_excel("artifacts/my_excel.xlsx")
 print("Done!!")
